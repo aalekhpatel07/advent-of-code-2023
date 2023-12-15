08bb27918e76c50e945b9a597b61c2aa5d00aef6
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2023-12-15 00:36:21 -0600

    Rustfmt fixes.
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- src/bin/day-14.rs
+++ src/bin/day-14.rs
@@ -1,75 +1,57 @@
-
 #[derive(Debug, Clone)]
 pub struct Grid2D(Vec<Vec<Rock>>);
-
 
 impl Grid2D {
     pub fn parse_str(data: &str) -> Self {
         Grid2D(
-            data
-            .lines()
-            .map(|line| {
-                line
-                .chars()
-                .map(|value| {
-                    value.into()
-                })
-                .collect()
-            })
-            .collect()
+            data.lines()
+                .map(|line| line.chars().map(|value| value.into()).collect())
+                .collect(),
         )
     }
 
     pub fn weight(&self) -> usize {
         self.0
-        .iter()
-        .rev()
-        .enumerate()
-        .map(|(row_index, row)| {
-            row.iter().filter(|&value| *value == Rock::Rounded).count() * (row_index + 1)
-        })
-        .sum()
+            .iter()
+            .rev()
+            .enumerate()
+            .map(|(row_index, row)| {
+                row.iter().filter(|&value| *value == Rock::Rounded).count() * (row_index + 1)
+            })
+            .sum()
     }
 
-    fn get_column(&self, index: usize) -> impl Iterator<Item=Rock> + '_ {
-        (0..self.0.len())
-        .map(move |row_index| self.0[row_index][index])
+    fn get_column(&self, index: usize) -> impl Iterator<Item = Rock> + '_ {
+        (0..self.0.len()).map(move |row_index| self.0[row_index][index])
     }
 
     fn shift_column_upward(&mut self, col_index: usize) {
-
         self.get_column(col_index);
         let _column = vec![Rock::Space; self.0.len()];
 
         let _next_free_spot = 0;
-
-        
-
     }
 }
 
-
 impl std::fmt::Display for Grid2D {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for row in self.0.iter().map(|row| {
-            row.iter()
-                .map(|&c| c.to_string())
-                .collect::<String>()
-        }) {
+        for row in self
+            .0
+            .iter()
+            .map(|row| row.iter().map(|&c| c.to_string()).collect::<String>())
+        {
             _ = writeln!(f, "{row}");
         }
         Ok(())
     }
 }
 
-
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Rock {
     Rounded,
     Cube,
-    Space
+    Space,
 }
-
 
 impl From<char> for Rock {
     fn from(value: char) -> Self {
@@ -77,22 +59,20 @@ impl From<char> for Rock {
             'O' => Self::Rounded,
             '#' => Self::Cube,
             '.' => Self::Space,
-            _ => panic!("invalid char")
+            _ => panic!("invalid char"),
         }
     }
 }
-
 
 impl std::fmt::Display for Rock {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match *self {
             Self::Rounded => write!(f, "O"),
             Self::Cube => write!(f, "#"),
-            Self::Space => write!(f, ".")
+            Self::Space => write!(f, "."),
         }
     }
 }
-
 
 pub fn main() {
     let data = include_str!("../../data/13.in");
@@ -102,8 +82,6 @@ pub fn main() {
 }
 
 pub fn solve_part1(_data: &str) -> usize {
-
-
     // data.split("\n\n")
     //     .par_bridge()
     //     .map(|block| {
@@ -120,30 +98,28 @@ pub fn solve_part1(_data: &str) -> usize {
     0
 }
 
-
 pub fn solve_part2(_data: &str) -> usize {
     0
-//     data.split("\n\n")
-//         .par_bridge()
-//         .map(|block| {
-//             let grid = Grid2D(
-//                 block
-//                     .lines()
-//                     .map(|row| row.chars().map(|c| (c as u8)).collect())
-//                     .collect(),
-//             );
-//             grid
-//         })
-//         .map(|grid| grid.find_new_line_of_reflection().unwrap())
-//         .map(|reflection| reflection.score())
-//         .sum()
+    //     data.split("\n\n")
+    //         .par_bridge()
+    //         .map(|block| {
+    //             let grid = Grid2D(
+    //                 block
+    //                     .lines()
+    //                     .map(|row| row.chars().map(|c| (c as u8)).collect())
+    //                     .collect(),
+    //             );
+    //             grid
+    //         })
+    //         .map(|grid| grid.find_new_line_of_reflection().unwrap())
+    //         .map(|reflection| reflection.score())
+    //         .sum()
 }
 
 #[cfg(test)]
 mod tests {
-    
 
-    use super::{solve_part1};
+    use super::solve_part1;
 
     #[test]
     fn smol() {
--- src/bin/day-15.rs
+++ src/bin/day-15.rs
@@ -1,6 +1,5 @@
 use regex::Regex;
 
-
 fn part1_hash(s: &str) -> usize {
     let mut current = 0;
     for c in s.chars().filter(|c| *c != '\n') {
@@ -10,17 +9,13 @@ fn part1_hash(s: &str) -> usize {
     current
 }
 
-
 fn solve_part2(data: &str) -> usize {
-
     const VAL: Vec<(String, usize)> = vec![];
-    let mut boxes : [Vec<(String, usize)>; 256] = [VAL; 256];
+    let mut boxes: [Vec<(String, usize)>; 256] = [VAL; 256];
 
     let pattern_re: Regex = Regex::new(r"(\w+)([-=])(\d)?").unwrap();
 
-    data
-    .split(',')
-    .for_each(|ins| {
+    data.split(',').for_each(|ins| {
         let caps = pattern_re.captures(ins).unwrap();
 
         let lens = caps.get(1).unwrap().as_str();
@@ -31,7 +26,10 @@ fn solve_part2(data: &str) -> usize {
         let valid_box = &mut boxes[box_index];
         if sign == "=" {
             let power: usize = caps.get(3).unwrap().as_str().parse().unwrap();
-            if let Some(found_index) = valid_box.iter().position(|(lens_in_box, _)| lens_in_box == &lens) {
+            if let Some(found_index) = valid_box
+                .iter()
+                .position(|(lens_in_box, _)| lens_in_box == &lens)
+            {
                 valid_box[found_index] = (lens.to_string(), power);
             } else {
                 valid_box.push((lens.to_string(), power));
@@ -42,14 +40,14 @@ fn solve_part2(data: &str) -> usize {
     });
 
     (0..256usize)
-    .map(|box_idx| {
-        boxes[box_idx]
-        .iter()
-        .enumerate()
-        .map(|(slot, (_, power))| (slot + 1) * *power * (box_idx + 1))
-        .sum::<usize>()
-    })
-    .sum()
+        .map(|box_idx| {
+            boxes[box_idx]
+                .iter()
+                .enumerate()
+                .map(|(slot, (_, power))| (slot + 1) * *power * (box_idx + 1))
+                .sum::<usize>()
+        })
+        .sum()
 }
 
 #[test]
@@ -60,10 +58,7 @@ fn test_sample() {
 }
 
 fn solve_part1(data: &str) -> usize {
-    data
-    .split(',')
-    .map(part1_hash)
-    .sum()
+    data.split(',').map(part1_hash).sum()
 }
 
 fn main() {
