af06f813b7fdd7a2e65ff98fb38c444b905d9a84
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2023-12-19 00:39:11 -0600

    Rustfmt fixes.
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- src/bin/day-19.rs
+++ src/bin/day-19.rs
@@ -1,17 +1,16 @@
-use std::{collections::{HashMap}, str::FromStr};
+use std::{collections::HashMap, str::FromStr};
 
+use indicatif::ParallelProgressIterator;
 use lazy_static::lazy_static;
 use rayon::prelude::*;
-use indicatif::ParallelProgressIterator;
 use regex::Regex;
 
-
 lazy_static! {
-    static ref RULE_CONDITIONAL_RE: Regex = Regex::new(r"(?<part>[xmas])(?<conditional>[<>])(?<threshold>\d+):(?<target>\w+)").unwrap();
+    static ref RULE_CONDITIONAL_RE: Regex =
+        Regex::new(r"(?<part>[xmas])(?<conditional>[<>])(?<threshold>\d+):(?<target>\w+)").unwrap();
     static ref WORKFLOW_RE: Regex = Regex::new(r"(?<id>\w+)\{(?<rules>.*)\}").unwrap();
     static ref RATING_RE: Regex = Regex::new(r"\{(?<ratings>.*)\}").unwrap();
 }
-
 
 #[derive(Debug, Clone)]
 pub struct Game {
@@ -25,14 +24,12 @@ impl FromStr for Game {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let (workflows, ratings) = s.split_once("\n\n").unwrap();
 
-        let workflows = 
-            workflows
+        let workflows = workflows
             .lines()
             .map(Workflow::from_str)
             .collect::<Result<Vec<_>, String>>()?;
 
-        let ratings = 
-            ratings
+        let ratings = ratings
             .lines()
             .map(Ratings::from_str)
             .collect::<Result<Vec<_>, String>>()?;
@@ -45,13 +42,12 @@ impl FromStr for Game {
 
         Ok(Self {
             ratings,
-            workflows: workflow_pool
+            workflows: workflow_pool,
         })
     }
 }
 
 pub fn solve_part1(data: &str) -> usize {
-
     let game = data.parse::<Game>().unwrap();
     let mut accepted_ratings = std::collections::HashSet::new();
 
@@ -70,35 +66,33 @@ pub fn solve_part1(data: &str) -> usize {
     }
 
     accepted_ratings
-    .into_iter()
-    .map(|rating| game.ratings[rating].inner.values().sum::<usize>())
-    .sum()
+        .into_iter()
+        .map(|rating| game.ratings[rating].inner.values().sum::<usize>())
+        .sum()
 }
 
 pub fn solve_part2(data: &str) -> usize {
-
     let game = data.parse::<Game>().unwrap();
 
     Ratings::distinct_combinations()
-    .par_bridge()
-    .progress_count(4_000 * 4_000 * 4_000 * 4_000)
-    .filter_map(|rating| {
-        let mut workflow = game.workflows.get("in").unwrap().clone();
-        loop {
-            let (accepted, maybe_next_workflow) = workflow.process(&rating);
-            let Some(next_workflow_id) = maybe_next_workflow else {
+        .par_bridge()
+        .progress_count(4_000 * 4_000 * 4_000 * 4_000)
+        .filter_map(|rating| {
+            let mut workflow = game.workflows.get("in").unwrap().clone();
+            loop {
+                let (accepted, maybe_next_workflow) = workflow.process(&rating);
+                let Some(next_workflow_id) = maybe_next_workflow else {
                 if accepted {
                     return Some(true)
                 } else {
                     return None;
                 }
             };
-            workflow = game.workflows.get(&next_workflow_id).unwrap().clone();
-        }
-    })
-    .count()
+                workflow = game.workflows.get(&next_workflow_id).unwrap().clone();
+            }
+        })
+        .count()
 }
-
 
 pub fn main() {
     let data = include_str!("../../data/19.in");
@@ -106,27 +100,27 @@ pub fn main() {
     println!("part 2: {}", solve_part2(data));
 }
 
-
 #[derive(Debug, Clone)]
 pub struct Ratings {
-    inner: HashMap<Part, usize>   
+    inner: HashMap<Part, usize>,
 }
 
 impl Ratings {
-    pub fn distinct_combinations() -> impl Iterator<Item=Self> {
-        (1..=4000)
-        .flat_map(|x| 
-            (1..=4000)
-            .flat_map(move |m| {
-                (1..=4000)
-                .flat_map(move |a| {
-                    (1..=4000)
-                    .map(move |s| {
-                        Ratings { inner: HashMap::from_iter(vec![(Part::X, x), (Part::M, m), (Part::A, a), (Part::S, s)])}
+    pub fn distinct_combinations() -> impl Iterator<Item = Self> {
+        (1..=4000).flat_map(|x| {
+            (1..=4000).flat_map(move |m| {
+                (1..=4000).flat_map(move |a| {
+                    (1..=4000).map(move |s| Ratings {
+                        inner: HashMap::from_iter(vec![
+                            (Part::X, x),
+                            (Part::M, m),
+                            (Part::A, a),
+                            (Part::S, s),
+                        ]),
                     })
                 })
             })
-        )
+        })
     }
 }
 
@@ -139,10 +133,8 @@ impl AsRef<HashMap<Part, usize>> for Ratings {
 impl FromStr for Ratings {
     type Err = String;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-
         let captures = RATING_RE.captures(s).expect("malformed ratings?");
-        let inner = 
-            captures
+        let inner = captures
             .name("ratings")
             .unwrap()
             .as_str()
@@ -155,73 +147,79 @@ impl FromStr for Ratings {
             })
             .collect::<HashMap<Part, usize>>();
 
-        Ok(Self {
-            inner
-        })
+        Ok(Self { inner })
     }
 }
-
 
 #[derive(Debug, Clone)]
 pub struct Workflow {
     id: WorkflowId,
-    rules: Vec<Rule>
+    rules: Vec<Rule>,
 }
 
 impl std::str::FromStr for Workflow {
     type Err = String;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-
         let captures = WORKFLOW_RE.captures(s).expect("malformed workflow?");
 
         let id = captures.name("id").unwrap().as_str().to_string();
         let rules = captures.name("rules").unwrap().as_str();
 
-        let rules = 
-            rules
+        let rules = rules
             .split(',')
             .map(Rule::from_str)
             .collect::<Result<Vec<_>, String>>()?;
 
-        Ok(Self {
-            id,
-            rules,
-        })
-    }   
+        Ok(Self { id, rules })
+    }
 }
 
 impl Workflow {
     pub fn process(&self, ratings: &Ratings) -> (bool, Option<WorkflowId>) {
-
         let mut index = 0;
 
         while index < self.rules.len() {
-
             match self.rules[index].clone() {
                 Rule::Conclude { accept } => return (accept, None),
-                Rule::ConcludeIfGreaterThan { part, accept, value } => {
+                Rule::ConcludeIfGreaterThan {
+                    part,
+                    accept,
+                    value,
+                } => {
                     if *ratings.inner.get(&part).unwrap() > value {
                         return (accept, None);
                     }
-                },
-                Rule::ConcludeIfLessThan { part, accept, value } => {
+                }
+                Rule::ConcludeIfLessThan {
+                    part,
+                    accept,
+                    value,
+                } => {
                     if *ratings.inner.get(&part).unwrap() < value {
                         return (accept, None);
                     }
-                },
+                }
                 Rule::Jump { target } => {
                     return (false, Some(target));
-                },
-                Rule::JumpIfGreaterThan { part, value, target } => {
+                }
+                Rule::JumpIfGreaterThan {
+                    part,
+                    value,
+                    target,
+                } => {
                     if *ratings.inner.get(&part).unwrap() > value {
                         return (false, Some(target));
                     }
-                },
-                Rule::JumpIfLessThan { part, value, target } => {
+                }
+                Rule::JumpIfLessThan {
+                    part,
+                    value,
+                    target,
+                } => {
                     if *ratings.inner.get(&part).unwrap() < value {
                         return (false, Some(target));
                     }
-                },
+                }
             }
             index += 1;
         }
@@ -236,24 +234,23 @@ pub enum Part {
     X,
     M,
     A,
-    S
+    S,
 }
-
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Rule {
-    JumpIfGreaterThan { 
+    JumpIfGreaterThan {
         part: Part,
         value: usize,
-        target: WorkflowId
+        target: WorkflowId,
     },
     JumpIfLessThan {
         part: Part,
         value: usize,
-        target: WorkflowId
+        target: WorkflowId,
     },
     Jump {
-        target: WorkflowId
+        target: WorkflowId,
     },
     ConcludeIfGreaterThan {
         part: Part,
@@ -267,7 +264,7 @@ pub enum Rule {
     },
     Conclude {
         accept: bool,
-    }
+    },
 }
 
 impl From<char> for Part {
@@ -277,51 +274,74 @@ impl From<char> for Part {
             'm' => Self::M,
             'a' => Self::A,
             's' => Self::S,
-            _ => panic!("unknown part?!")
+            _ => panic!("unknown part?!"),
         }
     }
 }
-
 
 impl std::str::FromStr for Rule {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-
         match RULE_CONDITIONAL_RE.captures(s) {
-            None => {
-                Ok(match s {
-                    "A" => Rule::Conclude { accept: true },
-                    "R" => Rule::Conclude { accept: false },
-                    _ => Rule::Jump { target: s.to_string() }
-                })
-            },
+            None => Ok(match s {
+                "A" => Rule::Conclude { accept: true },
+                "R" => Rule::Conclude { accept: false },
+                _ => Rule::Jump {
+                    target: s.to_string(),
+                },
+            }),
             Some(capture) => {
-                let part: Part = capture.name("part").unwrap().as_str().chars().next().unwrap().into();
-                let conditional = capture.name("conditional").unwrap().as_str().chars().next().unwrap();
-                let threshold = capture.name("threshold").unwrap().as_str().parse::<usize>().unwrap();
+                let part: Part = capture
+                    .name("part")
+                    .unwrap()
+                    .as_str()
+                    .chars()
+                    .next()
+                    .unwrap()
+                    .into();
+                let conditional = capture
+                    .name("conditional")
+                    .unwrap()
+                    .as_str()
+                    .chars()
+                    .next()
+                    .unwrap();
+                let threshold = capture
+                    .name("threshold")
+                    .unwrap()
+                    .as_str()
+                    .parse::<usize>()
+                    .unwrap();
                 let target = capture.name("target").unwrap().as_str();
 
                 match (conditional, target) {
-                    ('<', "A" | "R") => {
-                        Ok(Rule::ConcludeIfLessThan { part, accept: target == "A", value: threshold })
-                    },
-                    ('>', "A" | "R") => {
-                        Ok(Rule::ConcludeIfGreaterThan { part, accept: target == "A", value: threshold })
-                    },
-                    ('<', _) => {
-                        Ok(Rule::JumpIfLessThan { part, target: target.to_string(), value: threshold })
-                    },
-                    ('>', _) => {
-                        Ok(Rule::JumpIfGreaterThan { part, target: target.to_string(), value: threshold })
-                    },
-                    _ => Err(format!("Unknown case: {}", s))
+                    ('<', "A" | "R") => Ok(Rule::ConcludeIfLessThan {
+                        part,
+                        accept: target == "A",
+                        value: threshold,
+                    }),
+                    ('>', "A" | "R") => Ok(Rule::ConcludeIfGreaterThan {
+                        part,
+                        accept: target == "A",
+                        value: threshold,
+                    }),
+                    ('<', _) => Ok(Rule::JumpIfLessThan {
+                        part,
+                        target: target.to_string(),
+                        value: threshold,
+                    }),
+                    ('>', _) => Ok(Rule::JumpIfGreaterThan {
+                        part,
+                        target: target.to_string(),
+                        value: threshold,
+                    }),
+                    _ => Err(format!("Unknown case: {}", s)),
                 }
             }
         }
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -352,20 +372,27 @@ hdj{m>838:A,pv}
     }
     #[test]
     fn parse_rule() {
-        let rule_strs = vec![
-            "a<2006:qkq",
-            "m>2090:A",
-            "rfg",
-            "a>3333:R",
-            "R",
-            "A"
-        ];
+        let rule_strs = vec!["a<2006:qkq", "m>2090:A", "rfg", "a>3333:R", "R", "A"];
 
         let expected = vec![
-            Rule::JumpIfLessThan { part: Part::A, value: 2006, target: "qkq".to_string() },
-            Rule::ConcludeIfGreaterThan { part: Part::M, accept: true, value: 2090 },
-            Rule::Jump { target: "rfg".to_string() },
-            Rule::ConcludeIfGreaterThan { part: Part::A, accept: false, value: 3333 },
+            Rule::JumpIfLessThan {
+                part: Part::A,
+                value: 2006,
+                target: "qkq".to_string(),
+            },
+            Rule::ConcludeIfGreaterThan {
+                part: Part::M,
+                accept: true,
+                value: 2090,
+            },
+            Rule::Jump {
+                target: "rfg".to_string(),
+            },
+            Rule::ConcludeIfGreaterThan {
+                part: Part::A,
+                accept: false,
+                value: 3333,
+            },
             Rule::Conclude { accept: false },
             Rule::Conclude { accept: true },
         ];
@@ -374,6 +401,5 @@ hdj{m>838:A,pv}
             let rule = s.parse::<Rule>().unwrap();
             assert_eq!(rule, expected[idx]);
         }
-
     }
 }
