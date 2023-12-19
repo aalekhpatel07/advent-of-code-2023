58c5777ff2cb510988209e5a00e3992e6cfe51db
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2023-12-18 22:45:13 -0600

    Add solution for day 18.
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- src/bin/day-18.rs
+++ src/bin/day-18.rs
@@ -1,8 +1,32 @@
-use aoc_2023::data_structures::Polygon;
+use aoc_2023::data_structures::LateralPolygon;
 use regex::Regex;
 
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub fn main() {
+    let data = include_str!("../../data/18.in");
+    println!("part 1: {}", solve(data, false));
+    println!("part 2: {}", solve(data, true));
+}
+
+/// TIL: [Pick's Theorem] states that `A = i + b/2 - 1`
+/// where `i` denotes the number of lattice points interior to the polygon,
+/// `b` denotes the number of lattice points on the boundary of the polygon,
+/// and `A` denotes the area of the polygon.
+/// 
+/// The Shoelace formula gives us the area of the polygon, `A`.
+/// The perimeter `b` is simply the number of distinct lattice points on the horizontal/vertical edges of the polygon.
+/// 
+/// For this problem we want to calculate `i + b`, so using Pick's theorem, we have 
+/// the desired answer is `A + b / 2 + 1`.
+/// 
+/// [Pick's Theorem]: https://en.wikipedia.org/wiki/Pick%27s_theorem
+pub fn solve(data: &str, is_part2: bool) -> usize {
+
+    let polygon = parse_polygon(data, is_part2);
+    polygon.shoelace_area() + polygon.perimeter() / 2 + 1
+}
+
+
 pub enum Direction {
     Left,
     Up,
@@ -33,35 +57,50 @@ impl From<char> for Direction {
     }
 }
 
-pub fn solve_part1(data: &str) -> usize {
-
+pub fn parse_polygon(data: &str, is_part2: bool) -> LateralPolygon {
     let mut current_coord: (isize, isize) = (0, 0);
     let mut res = vec![current_coord];
 
-    let dig_plan_re = Regex::new(r"([DLRU]) (\d+) \(.*\)").unwrap();
+    let dig_plan_re = Regex::new(r"([DLRU]) (\d+) \((.*)\)").unwrap();
 
     data
     .lines()
     .for_each(|line| {
         let captures = dig_plan_re.captures(line).unwrap();
-
-        let dir: Direction = captures.get(1).unwrap().as_str().chars().next().unwrap().into();
-        let steps: isize = captures.get(2).unwrap().as_str().parse().unwrap();
+        let (dir, steps) = match is_part2 {
+            true => {
+                let color = captures.get(3).unwrap().as_str();
+                let hex_encoded_dist = usize::from_str_radix(&color[1..6], 16).unwrap();
+                let dir = match color[6..7].parse::<usize>().unwrap() {
+                    0 => Direction::Right,
+                    1 => Direction::Down,
+                    2 => Direction::Left,
+                    3 => Direction::Up,
+                    _ => {panic!("expected one of 0|1|2|3");}
+                };
+                (dir, hex_encoded_dist as isize)
+            },
+            false => {
+                let dir: Direction = captures.get(1).unwrap().as_str().chars().next().unwrap().into();
+                let steps: isize = captures.get(2).unwrap().as_str().parse().unwrap();
+                (dir, steps)
+            }
+        };
 
         let delta = dir.delta(steps);
         current_coord = (current_coord.0 + delta.0, current_coord.1 + delta.1);
         res.push(current_coord);
     });
 
-    res.remove(res.len() - 1);
-
-    let polygon = Polygon::new(res.into_iter());
-
-    polygon.area()
+    LateralPolygon::new(res.into_iter())   
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-pub fn main() {
+    #[test]
+    fn smol() {
     let data = r"R 6 (#70c710)
 D 5 (#0dc571)
 L 2 (#5713f0)
@@ -76,7 +115,7 @@ R 2 (#7807d2)
 U 3 (#a77fa3)
 L 2 (#015232)
 U 2 (#7a21e3)";
-
-    assert_eq!(solve_part1(data), 62);
-    // assert_eq!(solve_part2(data), 0);
+        assert_eq!(solve(data, false), 62);
+        assert_eq!(solve(data, true), 952408144115);
+    }
 }
--- src/data_structures/polygon.rs
+++ src/data_structures/polygon.rs
@@ -1,21 +1,69 @@
+
 pub type Coord = (isize, isize);
 
 
+/// A special case of a polygon where the edges
+/// are either horizontal or vertical but not slanted.
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Polygon {
-    // Assume they are listed in clockwise order.
-    coordinates: Vec<Coord>
+pub struct LateralPolygon {
+    /// The integer vertices of the polygon are listed in clockwise order.
+    pub coordinates: Vec<Coord>,
 }
 
 
-impl Polygon {
+impl LateralPolygon {
     pub fn new(coordinates: impl Iterator<Item=Coord>) -> Self {
         Self {
-            coordinates: coordinates.collect()
+            coordinates: coordinates.collect(),
         }
     }
 
-    pub fn area(&self) -> usize {
+    pub fn bounding_box(&self) -> (Coord, Coord) {
+        let x_min = self.coordinates.iter().map(|(x, _)| *x ).min().unwrap();
+        let x_max = self.coordinates.iter().map(|(x, _)| *x ).max().unwrap();
+        let y_min = self.coordinates.iter().map(|(_, y)| *y ).min().unwrap();
+        let y_max = self.coordinates.iter().map(|(_, y)| *y ).max().unwrap();
+        ((x_min, x_max), (y_min, y_max))
+    }
+
+    /// Return an iterator over the integer points that lie on the edges
+    /// of the polygon but not at the vertices.
+    pub fn edges(&self) -> impl Iterator<Item=Coord> + '_ {
+        self.coordinates
+        .as_slice()
+        .windows(2)
+        .flat_map(|window| {
+            let (prev_x, prev_y) = window[0];
+            let (curr_x, curr_y) = window[1];
+            let range = {
+                // Vertical line.
+                if prev_x == curr_x {
+                    // Don't include endpoints at all.
+                    prev_y.min(curr_y)+1..prev_y.max(curr_y)
+                }
+                // Horizontal line.
+                else if prev_y == curr_y {
+                    // Don't include endpoints at all.
+                    prev_x.min(curr_x)+1..prev_x.max(curr_x)
+                } else {
+                    panic!("not a standing/sleeping line");
+                }
+            };
+            range
+            .map(move |c| {
+                if prev_x == curr_x {
+                    (prev_x, c)
+                } else {
+                    (c, prev_y)
+                }
+            })
+        })
+    }
+
+    /// Compute the area of the polygon using the [Shoelace formula].
+    /// 
+    /// [Shoelace formula]: https://www.theoremoftheday.org/GeometryAndTrigonometry/Shoelace/TotDShoelace.pdf
+    pub fn shoelace_area(&self) -> usize {
 
         let mut coords_cycle = self.coordinates.clone();
         coords_cycle.push(coords_cycle[0]);
@@ -33,23 +81,41 @@ impl Polygon {
 
         left.abs_diff(right) / 2
     }
+
+    /// Get an iterator over all the unique integer points that form
+    /// the boundary of the polygon.
+    pub fn boundary(&self) -> impl Iterator<Item=Coord> + '_ {
+        // Edges don't include endpoints so we gotta chain em.
+        // This sweet optimization helps us avoid collecting everything
+        // into a HashSet when we want to return a unique set of points
+        // that form the boundary. In practise this changed my combined
+        // runtime of day-18 from 19s to 1.9ms (i.e. a 10000x speed up).
+        self
+        .edges()
+        .chain(self.coordinates.iter().copied())
+    }
+    /// Get the total number of unique integer points on the boundary of this polygon.
+    pub fn perimeter(&self) -> usize {
+        self.boundary().count()
+    }
+
 }
 
 #[cfg(test)]
 pub mod tests {
-    use super::Polygon;
+    use super::LateralPolygon;
 
     #[test]
     fn test_area_of_triangle() {
         let vertices = vec![(0, 0), (0, 5), (6, 0)];
-        let triangle = Polygon::new(vertices.into_iter());
-        assert_eq!(triangle.area(), 15);
+        let triangle = LateralPolygon::new(vertices.into_iter());
+        assert_eq!(triangle.shoelace_area(), 15);
     }
 
     #[test]
     fn test_area_of_square() {
         let vertices = vec![(0, 0), (0, 5), (5, 5), (5, 0)];
-        let triangle = Polygon::new(vertices.into_iter());
-        assert_eq!(triangle.area(), 25);
+        let triangle = LateralPolygon::new(vertices.into_iter());
+        assert_eq!(triangle.shoelace_area(), 25);
     }
 }
